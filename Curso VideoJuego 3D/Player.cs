cfcf80e9852488accbf4d9780fa83558a85274d4
César Bustamante <139395582+cesarbmathec@users.noreply.github.com>
2023-09-15 00:39:40 -0400

    Add files via upload

--- Curso VideoJuego 3D/Player.cs
+++ Curso VideoJuego 3D/Player.cs
@@ -12,6 +12,15 @@ public partial class Player : CharacterBody3D
 	private Vector3 lerpDirection;
 	private AnimationTree animationTree;
 	private Vector2 newDir;
+	private bool aiming = false;
+
+	[Export]
+	public Node3D weapon;
+
+	[Export]
+	public SkeletonIK3D leftIK;
+	[Export]
+	public SkeletonIK3D rightIK;
 
 	// Get the gravity from the project settings to be synced with RigidBody nodes.
 	public float gravity = ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle();
@@ -20,7 +29,7 @@ public partial class Player : CharacterBody3D
 
 	public override void _Ready()
 	{
-		lookAt = GetTree().GetNodesInGroup("CameraController")[0].GetNode<Node3D>("LookAt") as Node3D;
+		lookAt = GetTree().GetNodesInGroup("CameraController")[0].GetNode<Node3D>("LookAt");
 		animationTree = GetNode<AnimationTree>("AnimationTree");
 		animationTree.Active = true;
 	}
@@ -33,31 +42,40 @@ public partial class Player : CharacterBody3D
 		if (!IsOnFloor())
 		{
 			velocity.Y -= gravity * (float)delta;
-			animationTree.Set("parameters/Transition/transition_request", "Jumping");
+			animationTree.Set("parameters/TransitionStrafeJumping/transition_request", "Jumping");
+			animationTree.Set("parameters/Jumping/conditions/IsOnFloor", false);
 		}
 		else
 		{
-			animationTree.Set("parameters/Transition/transition_request", "Strafe");
+			animationTree.Set("parameters/TransitionStrafeJumping/transition_request", "Strafe");
+			animationTree.Set("parameters/Jumping/conditions/IsOnFloor", true);
+		}
+
+		if (Input.IsActionJustPressed("aiming"))
+		{
+			aiming = !aiming;
 		}
 
 		// Handle Jump.
 		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
 			velocity.Y = JumpVelocity;
 
-		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
 		Vector2 inputDir = Input.GetVector("left", "right", "up", "down");
 		Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
+
 		if (direction != Vector3.Zero)
 		{
 			velocity.X = Mathf.Lerp(velocity.X, direction.X * Speed * (float)delta * 15f, 0.25f);
 			velocity.Z = Mathf.Lerp(velocity.Z, direction.Z * Speed * (float)delta * 15f, 0.25f);
 
-			lerpDirection = lerpDirection.Lerp(new(
-				lookAt.GlobalPosition.X,
-				GlobalPosition.Y,
-				lookAt.GlobalPosition.Z
-			), 0.5f);
+			lerpDirection = lerpDirection.Lerp(
+				new(
+					lookAt.GlobalPosition.X,
+					GlobalPosition.Y,
+					lookAt.GlobalPosition.Z
+				),
+				0.35f
+			);
 
 			LookAt(lerpDirection);
 
@@ -71,7 +89,24 @@ public partial class Player : CharacterBody3D
 			newDir = newDir.Lerp(Vector2.Zero, 0.25f);
 		}
 
-		animationTree.Set("parameters/Strafe/blend_position", newDir);
+		if (aiming)
+		{
+			animationTree.Set("parameters/TransitionStrafeAiming/transition_request", "aiming");
+			animationTree.Set("parameters/StrafeAiming/blend_position", newDir);
+			animationTree.Set("parameters/Jumping/conditions/aiming", true);
+			leftIK.Start();
+			rightIK.Start();
+			weapon.Visible = true;
+		}
+		else
+		{
+			animationTree.Set("parameters/TransitionStrafeAiming/transition_request", "strafe");
+			animationTree.Set("parameters/Strafe/blend_position", newDir);
+			animationTree.Set("parameters/Jumping/conditions/aiming", false);
+			leftIK.Stop();
+			rightIK.Stop();
+			weapon.Visible = false;
+		}
 
 		Velocity = velocity;
 		MoveAndSlide();
