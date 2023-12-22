fc616fdca221bc46c170296ac171005eb2e80977
César Bustamante <139395582+cesarbmathec@users.noreply.github.com>
2023-12-21 23:50:42 -0400

    Add files via upload

--- Curso VideoJuego 3D/EnemyController.cs
+++ Curso VideoJuego 3D/EnemyController.cs
@@ -2,43 +2,74 @@ using Godot;
 
 public partial class EnemyController : CharacterBody3D
 {
+
+	// Variables de movimiento y física
 	public const float Speed = 5.0f;
 	public const float JumpVelocity = 4.5f;
-
 	private Vector3 direction;
+	public float gravity = ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle();
+	private Vector3 targetPoint; // Almacena la posición del target actual
 
+	// Variables para navegación
 	private NavigationAgent3D navigationAgent3D;
 
+	// Nodos extras
 	[Export]
-	public Node3D target;
+	public Node3D[] targetPoints; // Almacena un arreglo de los target
+	private CharacterBody3D player;
+	private Node3D eyes;
+	private AnimationTree animationTree;
 
-	// Get the gravity from the project settings to be synced with RigidBody nodes.
-	public float gravity = ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle();
+	// Variables para controlar los estados
+	public enum EnemyState
+	{
+		ALERT,
+		PATROL,
+		PURSUIT,
+		ATTACK,
+	}
+	[Export]
+	public EnemyState initialState = EnemyState.PATROL;
+	private EnemyState currentState;
 
 	public override void _Ready()
 	{
 		navigationAgent3D = GetNode<NavigationAgent3D>("NavigationAgent3D");
-		navigationAgent3D.TargetPosition = target.GlobalPosition;
+		player = GetTree().GetNodesInGroup("Player")[0] as CharacterBody3D;
+		eyes = GetNode<Node3D>("Eyes");
+		animationTree = GetNode<AnimationTree>("AnimationTree");
+
+		animationTree.Active = true;
+		// Establecemos el estado incial
+		currentState = initialState;
 	}
 
 	public override void _Process(double delta)
+	{
+		switch (currentState)
+		{
+			case EnemyState.PATROL:
+				PatrolState();
+				break;
+			case EnemyState.ALERT:
+				AlertState();
+				break;
+			case EnemyState.PURSUIT:
+				PursuitState();
+				break;
+			case EnemyState.ATTACK:
+				AttackState();
+				break;
+		}
+	}
+
+	public override void _PhysicsProcess(double delta)
 	{
 		Vector3 velocity = Velocity;
 
 		// Add the gravity.
 		if (!IsOnFloor())
 			velocity.Y -= gravity * (float)delta;
-
-		if (!(navigationAgent3D.IsNavigationFinished() && navigationAgent3D.IsTargetReached()))
-		{
-			LookAt(new Vector3(target.GlobalPosition.X, Position.Y, target.GlobalPosition.Z));
-			direction = navigationAgent3D.GetNextPathPosition() - GlobalPosition;
-			direction = direction.Normalized();
-		}
-		else
-		{
-			direction = Vector3.Zero;
-		}
 
 		if (direction != Vector3.Zero)
 		{
@@ -53,5 +84,50 @@ public partial class EnemyController : CharacterBody3D
 
 		Velocity = velocity;
 		MoveAndSlide();
+	}
+
+	// Métodos para controlar los estados del enemigo
+	public void PatrolState()
+	{
+		// Hemos llegado al punto de patrulla?
+		if (WeHaveArrived())
+		{
+			// Generamos un número aleatorio entero para establecer los puntos a patrullar
+			uint index = GD.Randi() % (uint)targetPoints.Length;
+			// Actualizamos el nuevo punto de patrulla
+			targetPoint = targetPoints[index].GlobalPosition;
+		}
+		// Movemos y rotamos el personaje
+		UpdateLookAtAndDirection(targetPoint);
+		MoveAndSlideAgent();
+
+		// Establecer las animaciones para este estado
+		animationTree.Set("parameters/conditions/idle", false);
+		animationTree.Set("parameters/conditions/running", true);
+		animationTree.Set("parameters/conditions/attack", false);
+	}
+	public void AlertState()
+	{ }
+	public void PursuitState()
+	{ }
+	public void AttackState()
+	{ }
+
+	// Métodos Auxiliares
+	public void UpdateLookAtAndDirection(Vector3 p)
+	{
+		navigationAgent3D.TargetPosition = p;
+		LookAt(new Vector3(p.X, Position.Y, p.Z));
+	}
+
+	public void MoveAndSlideAgent()
+	{
+		direction = navigationAgent3D.GetNextPathPosition() - GlobalPosition;
+		direction = direction.Normalized();
+	}
+
+	public bool WeHaveArrived()
+	{
+		return navigationAgent3D.IsNavigationFinished() && navigationAgent3D.IsTargetReached();
 	}
 }
