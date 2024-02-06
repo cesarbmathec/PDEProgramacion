d0086baf50bf9eb10cca2e978d9817c82114012b
César Bustamante <139395582+cesarbmathec@users.noreply.github.com>
2024-02-05 20:41:00 -0400

    Add files via upload

--- Recursos FPS/PlayerController.gd
+++ Recursos FPS/PlayerController.gd
@@ -13,11 +13,20 @@ extends CharacterBody3D
 @export_group("Weapon")
 @export var weapon_holder: Node3D
 @export var weapon_rotation_amount: float = 1
-@export var wobble_freq: float = 1
+@export var weapon_sway_amount: float = 1
 @export var wobble_amount: float = 1
+@export var wobble_freq: float = 1
 
-var def_weapon_holder_pos: Vector3
 var mouse_input: Vector2
+var weapon_holder_pos: Vector3
+
+enum player_state {
+	IDLE,
+	FIRE,
+	RELOAD,
+}
+
+var Current_state: player_state = player_state.IDLE
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -28,8 +37,9 @@ func _ready():
 	# Capturamos el cursor del mouse
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
-	# Establecemos la posición inicial del arma
-	def_weapon_holder_pos = weapon_holder.position
+	# Capturamos la posición del porta armas
+	if weapon_holder:
+		weapon_holder_pos = weapon_holder.position
 
 
 # Entradas
@@ -41,6 +51,16 @@ func _input(event):
 		cam_controller.rotation.x = clamp(cam_controller.rotation.x, -1.25, 1.5)
 		rotation.y -= event.relative.x * cam_speed
 		mouse_input = event.relative
+
+
+func _process(delta):
+	match Current_state:
+		player_state.IDLE:
+			idle()
+		player_state.FIRE:
+			fire(delta)
+		player_state.RELOAD:
+			reload()
 
 
 func _physics_process(delta):
@@ -70,14 +90,12 @@ func _physics_process(delta):
 	weapon_wobble(velocity.length(), delta)
 
 
-# Métodos Auxiliares:
-# Método para inclinar la cámara
+# Métodos auxiliares
 func cam_tilt(input_x, delta):
 	if cam_controller:
 		cam_controller.rotation.z = lerp(cam_controller.rotation.z, -input_x * cam_rotation_amount, 10 * delta)
 
 
-# Método para inclinar el arma
 func weapon_tilt(input_x, delta):
 	if weapon_holder:
 		weapon_holder.rotation.z = lerp(weapon_holder.rotation.z, -input_x * weapon_rotation_amount, 10 * delta)
@@ -85,21 +103,32 @@ func weapon_tilt(input_x, delta):
 
 func weapon_sway(delta):
 	mouse_input = lerp(mouse_input, Vector2.ZERO, delta)
-	weapon_holder.rotation.x = lerp(weapon_holder.rotation.x, mouse_input.y * weapon_rotation_amount, delta)
-	weapon_holder.rotation.y = lerp(weapon_holder.rotation.y, mouse_input.x * weapon_rotation_amount, delta)
+	weapon_holder.rotation.x = lerp(weapon_holder.rotation.x, mouse_input.y * weapon_sway_amount, 10 * delta)
+	weapon_holder.rotation.y = lerp(weapon_holder.rotation.y, mouse_input.x * weapon_sway_amount, 5 * delta)
 
 
 func weapon_wobble(vel: float, delta):
 	if weapon_holder:
-		if vel > 0:
+		if vel > 0 and is_on_floor():
 			weapon_holder.position.y = lerp(
-				weapon_holder.position.y, def_weapon_holder_pos.y + sin(Time.get_ticks_msec() * wobble_freq) * wobble_amount, 10 * delta
+				weapon_holder.position.y, weapon_holder_pos.y + sin(Time.get_ticks_msec() * wobble_freq) * wobble_amount, 10 * delta
 			)
 			weapon_holder.position.x = lerp(
-				weapon_holder.position.x,
-				def_weapon_holder_pos.x + sin(Time.get_ticks_msec() * wobble_freq * 0.5) * wobble_amount,
-				10 * delta
+				weapon_holder.position.x, weapon_holder_pos.x + sin(Time.get_ticks_msec() * wobble_freq * 0.5) * wobble_amount, 10 * delta
 			)
 		else:
-			weapon_holder.position.y = lerp(weapon_holder.position.y, def_weapon_holder_pos.y, 10 * delta)
-			weapon_holder.position.x = lerp(weapon_holder.position.x, def_weapon_holder_pos.x, 10 * delta)
+			weapon_holder.position.y = lerp(weapon_holder.position.y, weapon_holder_pos.y, 10 * delta)
+			weapon_holder.position.x = lerp(weapon_holder.position.x, weapon_holder_pos.x, 10 * delta)
+
+
+# Métodos para controlar los estados del player
+func idle():
+	pass
+
+
+func fire(_delta):
+	pass
+
+
+func reload():
+	pass
